5991acaf3fe12dbf7b64028052daab5e2ba18296
albertCoder <jkdev123cool@gmail.com>
2020-08-07 20:32:52 +0800

    delete test code

--- Example/JKUtilityKit/JKViewController.m
+++ Example/JKUtilityKit/JKViewController.m
@@ -19,11 +19,6 @@
 {
     [super viewDidLoad];
 	
-    
-    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(2 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-        
-        [JKUtility jumpToSetting];
-    });
 }
 
 - (void)didReceiveMemoryWarning
